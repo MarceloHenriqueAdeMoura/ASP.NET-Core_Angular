6786dd5c395f84448f4e11941c1d71597cf4eb64
Marcelo Henrique <marcelo_assis98@hotmail.com>
2020-04-28 17:26:36 -0300

    Add model Evento

---
+++ ProjectAngular/Models/Evento.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectAngular.Models
+{
+    public class Evento
+    {
+        public int Id { get; set; }
+        public string Tema { get; set; }
+        public string Local { get; set; }
+        public DateTime DataEvento { get; set; }
+        public int QtdPessoas { get; set; }
+        public string Lote { get; set; }
+        public Evento(int id, string tema, string local, DateTime dataEvento, int qtdPessoas, string lote)
+        {
+            Id = id;
+            Tema = tema;
+            Local = local;
+            DataEvento = dataEvento;
+            QtdPessoas = qtdPessoas;
+            Lote = lote;
+        }
+    }
+}
